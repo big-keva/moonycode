84ff1d637a4de6ece10b362822cf1a04b61d4e16
Keva <keva@rambler.ru>
2026-03-17 20:48:06 +0300

    classic library path variables

--- cmake/MoonyCodeConfig.cmake
+++ cmake/MoonyCodeConfig.cmake
@@ -11,12 +11,4 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MoonyCode
     REQUIRED_VARS MoonyCode_LIBRARY MoonyCode_INCLUDE_DIR)
 
-if(MoonyCode_FOUND AND NOT TARGET Moony::Code)
-    add_library(Moony::Code UNKNOWN IMPORTED)
-
-    set_target_properties(Moony::Code PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${MoonyCode_INCLUDE_DIR}"
-        IMPORTED_LOCATION "${MoonyCode_LIBRARY}")
-endif()
-
 mark_as_advanced(MoonyCode_INCLUDE_DIR MoonyCode_LIBRARY)
