7a26812f377cd62456c6143f80e345069199a9c6
Keva <keva@rambler.ru>
2026-03-19 19:21:43 +0300

    vars renamed

--- cmake/MoonyCodeConfig.cmake
+++ cmake/MoonyCodeConfig.cmake
@@ -1,14 +1,14 @@
-find_path(MoonyCode_INCLUDE_DIR
+find_path(MoonyCode_INCLUDE
     NAMES codes.h chartype.h
     PATHS /usr/local/include/moonycode /usr/include/moonycode)
 
-find_library(MoonyCode_LIBRARY
+find_library(MoonyCode_LIB
     NAMES moonycode
     PATHS /usr/local/lib /usr/lib)
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(MoonyCode
-    REQUIRED_VARS MoonyCode_LIBRARY MoonyCode_INCLUDE_DIR)
+    REQUIRED_VARS MoonyCode_LIB MoonyCode_INCLUDE)
 
-mark_as_advanced(MoonyCode_INCLUDE_DIR MoonyCode_LIBRARY)
+mark_as_advanced(MoonyCode_INCLUDE MoonyCode_LIB)
